8027d146b5a48620296b763382601b46a1366b22
julianxrenner <julesman18@gmail.com>
2026-01-18 09:17:14 -0500

    tried having multiple requests in the same file

--- index.js
+++ index.js
@@ -46,9 +46,30 @@ app.get('/', (request, response) => {
     response.status(204).end()
   })
 
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+  
   app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
     response.json(note)
   })
 
