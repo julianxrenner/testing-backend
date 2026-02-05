2a84b1c38cece6f9ec54a065dae8f3ca368c4a13
julianxrenner <julesman18@gmail.com>
2026-02-05 05:44:50 -0500

    trying to test deployment

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
   }
-
   next(error);
 };
 
